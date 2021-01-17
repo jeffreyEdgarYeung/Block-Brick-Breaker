737f980ee4558f23e3f21b9892f85bee2bf5e882
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-01-17 13:41:40 -0800

    Win condition/level progression

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int bricksLeft = 0;
+    SceneLoader sceneLoader;
+
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBrick()
+    {
+        bricksLeft++;
+    }
+
+    public void RemoveBrick()
+    {
+        bricksLeft--;
+        if (bricksLeft == 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+
+}

cef5bfc8880bbfd7ddc753e9b1d0aaffea472b95
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-01-16 19:22:32 -0800

    Paddle Movement/Collision

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthUnits = 16f;
+    [SerializeField] float minX = 1.23f;
+    [SerializeField] float maxX = 14.76f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXPosition = (Input.mousePosition.x / Screen.width * screenWidthUnits);
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mouseXPosition, minX, maxX);
+        transform.position = paddlePosition;
+    }
+}

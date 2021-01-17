6af396c791a500db3a67dbf55556bc34f6d5a0c4
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-01-17 13:43:17 -0800

    Level brick count / removal

--- Assets/Scripts/BlockBrick.cs
+++ Assets/Scripts/BlockBrick.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class BlockBrick : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSFX;
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBrick();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSFX, Camera.main.transform.position, .08f);
+        level.RemoveBrick();
         Destroy(gameObject);
     }
 }

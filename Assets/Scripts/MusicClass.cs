590939fb106dfbf1eba73133ea95df278b359238
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-01-18 11:41:15 -0800

    Gameplay/Title Music

---
+++ Assets/Scripts/MusicClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicClass : MonoBehaviour
+{
+    private AudioSource audioSource;
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+        Destroy(gameObject);
+    }
+}
